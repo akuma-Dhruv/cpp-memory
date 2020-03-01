8fa916e7010fd3ccfff7f0721a289a54b986f4b2
Dhruv Garg <60261904+akuma-Dhruv@users.noreply.github.com>
2020-03-01 23:17:58 +0530

    #define

---
+++ macro.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#define MULTIPLY(a, b) a*b
+
+int main(){
+
+    cout<<MULTIPLY(2,2)<<endl;
+    cout<<MULTIPLY(0+5,5+0)<<endl;
+    cout << MULTIPLY(2+3, 5)<<endl;
+    cout << MULTIPLY(5+4,3+5)<<endl;
+    return 0;
+}
